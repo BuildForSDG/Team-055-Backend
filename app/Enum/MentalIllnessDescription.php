431329d185052ef7fbd2f12c3c9f8e7c7a2295b3
rheedwahn <ridwan2bus@gmail.com>
2020-06-18 14:09:09 +0100

    personalize description

--- app/Enum/MentalIllnessDescription.php
+++ app/Enum/MentalIllnessDescription.php
@@ -5,11 +5,11 @@ namespace App\Enum;
 class MentalIllnessDescription
 {
     public static $levels = [
-        'low_level' => 'The user may not necessarily need medical attention but can be advised to talk to someone close
+        'low_level' => 'You may not necessarily need medical attention but can be advised to talk to someone close
                         and look up more about the mental illness',
-        'mid_level' => 'The user may be currently experiencing symptoms of moderate illness. The results doesn’t mean 
+        'mid_level' => 'You may be currently experiencing symptoms of moderate illness. The results doesn’t mean 
                         that the user is sick but this symptoms could be causing difficulties managing relationships 
                         and even everyday task',
-        'high_level' => 'The user need urgent medical attention and could be Suicidal at this point'
+        'high_level' => 'You need urgent medical attention and could be Suicidal at this point'
     ];
 }
